babcf8f16c0b3376d085b6856e180a9bdecb5775
Julius Niyonzima <Juliusniyonzima423@gmail.com>
2025-02-27 23:54:11 -0600

    Add api config file in the API folder

---
+++ api/apiConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    OSINTAPIKEY : 'enter api keys', //secure the keys
+    rooturl: "url", //root url for the OSINT API
+    endpoints:
+        {
+            search: '/domain search',
+            details: '/fectchresult'
+        }
+
+};
