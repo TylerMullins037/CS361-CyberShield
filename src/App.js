5a2074f5b38dcaa064fec4c56183cf57d88f3da1
TylerMullins037 <144743073+TylerMullins037@users.noreply.github.com>
2025-02-27 20:15:10 -0600

    authorization
    ensures that you can't bypass login by typing page URLs in

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-// App.js
-import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
- 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./AuthContext";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import ProtectedRoute from "./ProtectedRoute";
+
 const App = () => {
-   return (
-      <>
-         <Routes> 
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-         </Routes>
-      </>
-   );
+  return (
+    <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route 
+            path="/dashboard" 
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            } 
+          />
+        </Routes>
+    </AuthProvider>
+  );
 };
- 
+
 export default App;
