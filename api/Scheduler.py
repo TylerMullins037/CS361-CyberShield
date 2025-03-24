7c783a3de15b2471e664661aab9ea2efd3ba46f0
TylerMullins037 <144743073+TylerMullins037@users.noreply.github.com>
2025-03-24 06:59:17 -0500

    Update Scheduler.py

--- api/Scheduler.py
+++ api/Scheduler.py
@@ -1,47 +1,22 @@
-import schedule
+import os
 import time
-import SecurityTrails
-from fetch_osint import fetch_data, store_threat_intelligence, security_data,virus_data_fetch
+import logging
+import schedule
+from fetch_osint import fetch_osint_updates
 
-def run_osint_updates():
-    try:
-        data_available = fetch_data() #
-        #iterate through fetched data
-        for  data in data_available:
-            threat_name = data.get('threat_name')
-            vulnerability = data.get('vulnerability')
-            likelihood = data.get('likelihood')
-            impact = data.get('impact')
-            #store them in the database
-            store_threat_intelligence(threat_name,vulnerability, likelihood, impact)
-            #information feteched by security trails
-        security_trials_data = security_data()
-        for data in security_trials_data:
-            name = data.get('threat_name')
-            vulnerability = data.get('vulnerability')
-            likelihood = data.get('likelihood')
-            impact = data.get('impact')
-            store_threat_intelligence(name, vulnerability, likelihood, impact)
+#configuring logging display levels of warning, critical, Error with time stamps
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", force=True)
 
-        domain = SecurityTrails.DOMAIN
-        virus_data = virus_data_fetch(domain)
-        if not virus_data:
-            print(f"No virus found in this {domain} domain")
-        for subdomain in virus_data:
-            threat_name = subdomain.get("VirusTotalDomain", "Unknown threat")
-            vulnerability = subdomain.get("Subdomain vulnerability", "unknown further investigation needed" )
-            likelihood = subdomain.get("likelihood", "Medium" ) #likelihood
-            impact = subdomain.get("impact", "High")  #impact
-            #get the threat name
-            print(f"The list name of the threat is {threat_name}")
-            store_threat_intelligence(threat_name, vulnerability, likelihood, impact)
+#schedule to run every 6 hours
+schedule.every(6).hours.do(fetch_osint_updates)
+if __name__=="__main__":
+    logging.info("OSINT Threat intelligence monitoring started...")
+    while True:
+        try:
+            #check for schedule tasks
+            logging.info("Checking Scheduled tasks...")
+            schedule.run_pending()
+            time.sleep(60) # check every 60 seconds
+        except Exception as e:
+            logging.error(f"Error in scheduler loop: {e}")
 
-        print(f"successfully processed {len(virus_data)} threat intelligence data")
-
-    except Exception as e:
-        print(f'Error fetching the threat updates {e}')
-# Schedule API calls every 6 hours
-schedule.every(6).hours.do(run_osint_updates)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
