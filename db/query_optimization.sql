21dda37fd5e5e3de3e79cc870fc26cd442e88c1c
TylerMullins037 <144743073+TylerMullins037@users.noreply.github.com>
2025-04-15 11:33:12 -0500

    Update query_optimization.sql

--- db/query_optimization.sql
+++ db/query_optimization.sql
@@ -1,1 +1,10 @@
 EXPLAIN ANALYZE SELECT * FROM tva_mapping WHERE risk_score >20;
+
+
+SELECT 
+    t.threat_name,
+    ms.mitigation_strategy
+FROM 
+    tva_mapping t
+JOIN 
+    mitigation_strategies ms ON t.id = ms.tva_mapping_id
